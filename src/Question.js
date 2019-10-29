14a1a8d2d86eca7db4f75820e97ebac6c24077c3
Levy Klots <levyklots@gmail.com>
2019-10-29 08:45:11 -0700

    ui nits

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 
 import Grid from '@material-ui/core/Grid';
-import {useSpring, useTrail, animated, useChain} from 'react-spring'
+import {useSpring, useTrail, animated, useChain} from 'react-spring';
 
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -14,13 +14,13 @@ export default function Question({qid, pics, choices, answer, onSelected }) {
   const animateContainerRef = useRef();
   const animateContainerProps = useSpring({
     ref: animateContainerRef,
-    from: { marginTop: -200, opacity: 0, transform: 'translate3d(0, -40px, 0)' },
-    to: { marginTop: 0, opacity: 1, transform: 'translate3d(0, 0px, 0)' }
+    from: { marginLeft: 20, opacity: 0, transform: 'translate3d(40, 0px, 0)' },
+    to: { marginLeft: 0, opacity: 1, transform: 'translate3d(0, 0px, 0)' }
   });
   const animateChoicesRef = useRef();
   const animateChoicesProps = useTrail(choices.length, {
     ref: animateChoicesRef,
-    from: { marginLeft: -20, opacity: 0, transform: 'translate3d(0, -40px, 0)' },
+    from: { marginLeft: -20, opacity: 0, transform: 'translate3d(40px, 0px, 0)' },
     to: { marginLeft: 0, opacity: 1, transform: 'translate3d(0, 0px, 0)' }
   });
   useChain([animateContainerRef, animateChoicesRef], [0, 0.2]);
